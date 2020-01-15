bd5eff7c8e85ab0953764a439b6a324790158bac
Davis Hyer <dhyer@instructure.com>
2020-01-15 13:40:54 -0700

    clean up swagger docs
    Overrides swagger defaults, only presenting what's necessary
    to play the game.

--- src/main/kotlin/com/dhyer/bot_fighter/SwaggerConfig.kt
+++ src/main/kotlin/com/dhyer/bot_fighter/SwaggerConfig.kt
@@ -4,8 +4,10 @@ import com.dhyer.bot_fighter.annotations.Private
 import com.google.common.base.Predicates
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
@@ -15,8 +17,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2
 class SwaggerConfig {
   @Bean
   fun api(): Docket = Docket(DocumentationType.SWAGGER_2)
+                        .apiInfo(apiInfo())
+                        .useDefaultResponseMessages(false)
                         .select()
                         .apis(Predicates.not(RequestHandlerSelectors.withMethodAnnotation(Private::class.java)))
                         .paths(Predicates.not(PathSelectors.regex("/(error|health-check).*")))
                         .build()
+
+  fun apiInfo(): ApiInfo {
+    return ApiInfoBuilder()
+            .title("Bot-Fighter API")
+            .description("for questions, ping @dhyer in slack")
+            .build()
+  }
 }
